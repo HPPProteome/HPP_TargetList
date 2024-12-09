2372bed31800d4be81fb74aa7b7f659529e4f061
lightningstar425 <lightning427@gmail.com>
2024-12-09 10:31:50 -0800

    Includes annotations in secound sheet for data columns

--- build_pipeline/fasta_builder.py
+++ build_pipeline/fasta_builder.py
@@ -78,7 +78,7 @@ gene_df['Hydrophobicity'] = ''
 gene_df['PI'] = ''
 
 gene_df = gene_df.sort_values(by='UniProtKB ID', na_position='first')
-
+Ucount = 0
 print("Calculating Hydrophobicity and PI")
 #Gets sequence if not present
 for index, row in gene_df.iterrows():
@@ -89,6 +89,8 @@ for index, row in gene_df.iterrows():
 		print("Not present:", row['Gene ID'], row['Gene Symbol'])
 	
 	else:
+		if 'U' in gene_df.at[index, 'sequence']:
+			Ucount += 1
 
 		#Removes U becuase protein analysis can't handle U.
 		sequence  = ''.join([aa for aa in gene_df.at[index, 'sequence'] if aa in "ACDEFGHIKLMNPQRSTVWY"])
@@ -97,19 +99,84 @@ for index, row in gene_df.iterrows():
 		gene_df.at[index, 'Hydrophobicity'] = round(analysis.gravy(), 3)
 		gene_df.at[index, 'PI'] = round(analysis.isoelectric_point(), 3)
 
-
+print("Number of sequences with U:", Ucount)
 
 print("Writing FASTA file")
 with open('coding_genes.fasta', 'w') as f:
     f.write(''.join(gene_df.apply(to_fasta, axis=1)))
 print("File written as coding_genes.fasta")
 
+
+#Includes annotations for what each column means
+secound_sheet = {"Gene ID": "ENSG number, from GENCODE GTF.",
+    "Gene Symbol": "Gene Symbol, from GENCODE GTF.",
+    "Chromosome": "Chromosome location, from GENCODE GTF.",
+    "Start": "Nucleotide the gene starts on, from GENCODE FASTA.",
+    "End": "Nucleotide the gene ends on, from GENCODE FASTA.",
+    "Translation Type": (
+        "Represents how a transcript was chosen for a single gene, from GENCODE GTF. "
+        "Main Select - Indicates transcript is the single agreed-upon transcript for the protein-coding gene. "
+        "Canonical - Indicates it was the 'best' transcript, selected by an individual annotation source."
+    ),
+    "CDS Length": "Length of gene’s amino acid sequence, from GENCODE FASTA.",
+    "ENSP & ENST": "Identifiers taken from UniProtKB. If unavailable, they are taken from GENCODE FASTA.",
+    "UniProtKB ID": "Protein Coding Gene UniProtKB ID, taken from UniProtKB.",
+    "Reviewed": "Status of entry taken from UniProtKB that matches the gene’s assigned UniProtKB ID.",
+    "Reviewed Entry Available": (
+        "Indicates that even if a UniProtKB entry with an unreviewed ENSG link was chosen, "
+        "a reviewed UniProtKB entry with a gene symbol link is available in False_ensg_over_name.xlsx."
+    ),
+    "Entry Name": "Gene entry name, from UniProtKB tsv.",
+    "Uniprot Symbol": "UniProtKB ID, from UniProtKB tsv.",
+    "Description": "Description of UniProtKB entry, from UniProtKB tsv.",
+    "Protein Length": "Length of amino acid sequence, from UniProtKB tsv.",
+    "PE": (
+        "Level of protein existence (1: Evidence at protein level, 2: Evidence at transcript level, "
+        "3: Inferred from homology, 4: Protein predicted, and 5: Protein Uncertain), from UniProtKB."
+    ),
+    "Suggested PE": (
+        "Using information from NextProt, if any tissue for a protein coding gene has a score over 1.0, "
+        "it suggests a change to 2 if PE score is >2."
+    ),
+    "Highest nTPM Score": "Provides the highest nTPM score for a gene, from NextProt.",
+    "Tissues with nTPM Score Above 1": (
+        "Counts the number of tissues with a nTPM score above 1.0. Out of 50 tissues, from NextProt."
+    ),
+    "Link Made Through": (
+        "Indicates whether the UniProtKB entry was linked to the GENCODE entry through the ENG number (gene_id) or its symbol (gene_symbol)."
+    ),
+    "Isoform": "Notes the specific isoform linked to the ENSG number if present, from UniProtKB tsv.",
+    "Difference in Lengths": (
+        "Finds the difference in length between the UniProtKB and GENCODE entries’ amino acid sequence."
+    ),
+    "EC Number": "EC number of the gene, from UniProtKB tsv.",
+    "Num Transmembrane Regions": "Number of transmembrane regions, from UniProtKB tsv.",
+    "Signal Peptide": "Length of a signal peptide if present, from UniProtKB tsv.",
+    "PeptideAtlas Category": "Category of the gene, from PeptideAtlas.",
+    "Observed": "Number of observed peptides, from PeptideAtlas.",
+    "Distinct": "Number of distinct peptides, from PeptideAtlas.",
+    "Uniquely Mapping": "Number of uniquely mapping peptides, from PeptideAtlas.",
+    "Hydrophobicity": "Hydrophobicity of amino acid sequence for given gene, calculated with BioPython’s ProteinAnalysis.",
+    "PI": "PI number of amino acid sequence for given gene, calculated with BioPython’s ProteinAnalysis."
+}
+
+
+
+
+secound_sheet_df = pd.DataFrame(list(secound_sheet.items()), columns=["Column", "Description"])
+
+
+
 print("Making updated table")
 
 gene_df['Chromosome'] = gene_df['Chromosome'].apply(number)
 gene_df = gene_df.sort_values(by='Chromosome', ascending=True)
 gene_df['Chromosome'] = gene_df['Chromosome'].replace(25, "M").replace(24,"Y").replace(23, "X")
-
 gene_df.drop('sequence', axis=1, inplace=True)
-gene_df.to_excel("Supplemental_table_1.xlsx", index=False)
+
+output_file = "Supplemental_table_1.xlsx"
+with pd.ExcelWriter(output_file, engine="openpyxl") as writer:
+    gene_df.to_excel(writer, sheet_name="Gene Table", index=False)
+    secound_sheet_df.to_excel(writer, sheet_name="Column Annotations", index=False)
+
 print("Done")
