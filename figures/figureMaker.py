fd343261328dbee3c4070a2e29e6cd3aa0f14525
lightningstar425 <lightning427@gmail.com>
2025-02-27 15:14:29 -0800

    Updated figure maker for best chart looks

--- figures/figureMaker.py
+++ figures/figureMaker.py
@@ -1,26 +1,20 @@
-#Libraries
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 
-#Function only displays percents higher than 3% on pie chart
 def percent(num):
         if num > 3:
             return str(round(num, 1)) + '%'
         else:
             return ''
 
-
-
-import matplotlib.pyplot as plt
-
 # Bar chart of PeptideAtlas categories
 peptideatlas_categories = {
     "N/A (entry not found)": 2,
     "Not Detected": 19,
+    "Weak": 2,
     "Subsumed": 20,
+    "Marginally Distinguished": 8,
     "Identical": 6,
     "Indistinguishable": 2,
-    "Marginally Distinguished": 8,
-    "Weak": 2,
     "Canonical": 17,
 }
 
@@ -29,9 +23,10 @@ data = list(peptideatlas_categories.values())
 
 # Pie chart of Suggested PE Values
 Suggested_PE_Values = {
-    "1": 18,
-    "2": 15,
-    "5": 3,
+    "PE1": 17,
+    "PE2": 15,
+    "PE3": 41,
+    "PE5": 3,
 }
 data_pe = list(Suggested_PE_Values.values())
 label_pe = list(Suggested_PE_Values.keys())
@@ -75,7 +70,8 @@ label_hpa = list(hpa_categories.keys())
 alphafold_plddt_scores = {
     "N/A (entry not found)": 2,
     "0-50 (Very Low)": 11,
-    "50-70 (Low)": 32,
+    "50-60 (Low)" : 10,
+    "60-70 (Low)" : 22,
     "70-80 (High)": 8,
     "80-90 (High)": 15,
     "90-100 (Very High)": 8
@@ -91,15 +87,16 @@ plt.barh(label, data, color="darkblue")
 plt.xlabel("Protein Entries", fontsize=15)
 plt.title("PeptideAtlas Categories", fontsize=25)
 ax = plt.gca()
-plt.text(-0.08, 1.0, "a", transform=ax.transAxes, fontsize=18, fontweight="bold")
-plt.yticks(fontsize=10)
+plt.text(-0.08, 1.0, "a", transform=ax.transAxes, fontsize=28, fontweight="bold")
+plt.xticks(fontsize=15)
+plt.yticks(fontsize=12)
 ax.xaxis.set_major_formatter(plt.FuncFormatter(lambda x, _: int(x) if x.is_integer() else ""))
 for spine in ["top", "right"]:
     ax.spines[spine].set_visible(False)
 
 # Suggested PE Values Pie Chart
-plt.subplot(2, 3, 4)
-wedges, texts, autotexts = plt.pie(data_pe, labels=label_pe, autopct='%.1f%%', colors=["mediumseagreen", "mediumslateblue", "brown"], startangle=90, pctdistance=0.75)
+plt.subplot(2, 3, 6)
+wedges, texts, autotexts = plt.pie(data_pe, labels=label_pe, autopct='%.1f%%', colors=["mediumseagreen", "mediumslateblue", "lightseagreen", "brown"], startangle=90, pctdistance=0.75)
 for text in texts:
     text.set_fontsize(15)
 for autotext in autotexts:
@@ -107,27 +104,28 @@ for autotext in autotexts:
 plt.title("Suggested PE Values", fontsize=25)
 plt.yticks(fontsize=10)
 ax = plt.gca()
-plt.text(-0.08, 1.0, "d", transform=ax.transAxes, fontsize=18, fontweight="bold")
+plt.text(-0.08, 1.0, "f", transform=ax.transAxes, fontsize=28, fontweight="bold")
 
 # Assessment Categories Pie Chart
-plt.subplot(2, 3, 6)
+plt.subplot(2, 3, 5)
 wedges, texts, autotexts = plt.pie(data_ac, labels=label_ac, autopct=percent, colors=["lime", "olivedrab", "steelblue", "coral", "yellow", "red"], startangle=90, pctdistance=0.7)
 for text in texts:
     text.set_fontsize(15)
 for autotext in autotexts:
     autotext.set_fontsize(13)
 ax = plt.gca()
-plt.text(-0.08, 1.0, "f", transform=ax.transAxes, fontsize=18, fontweight="bold")
+plt.text(-0.08, 1.0, "e", transform=ax.transAxes, fontsize=28, fontweight="bold")
 plt.title("Assessment Categories", fontsize=25)
 
 # Publications Count Bar Chart
-plt.subplot(2, 3, 5)
+plt.subplot(2, 3, 4)
 plt.bar(label_pub, data_pub, color="indigo")
 plt.xlabel("Number of Publications", fontsize=15)
 plt.ylabel("Protein Entries", fontsize=15)
-plt.yticks(fontsize=10)
+plt.yticks(fontsize=12)
+plt.xticks(fontsize=15)
 ax = plt.gca()
-plt.text(-0.08, 1.0, "e", transform=ax.transAxes, fontsize=18, fontweight="bold")
+plt.text(-0.08, 1.0, "d", transform=ax.transAxes, fontsize=28, fontweight="bold")
 for spine in ["top", "right"]:
     ax.spines[spine].set_visible(False)
 
@@ -135,10 +133,11 @@ for spine in ["top", "right"]:
 plt.subplot(2, 3, 3)
 plt.barh(label_hpa, data_hpa, color="maroon")
 plt.xlabel("Protein Entries", fontsize=15)
-plt.title("HPA Categories", fontsize=20)
-plt.yticks(fontsize=10)
+plt.title("HPA RNA Expression Categories", fontsize=20)
+plt.yticks(fontsize=12)
+plt.xticks(fontsize=15)
 ax = plt.gca()
-plt.text(-0.08, 1.0, "c", transform=ax.transAxes, fontsize=18, fontweight="bold")
+plt.text(-0.08, 1.0, "c", transform=ax.transAxes, fontsize=28, fontweight="bold")
 for spine in ["top", "right"]:
     ax.spines[spine].set_visible(False)
 
@@ -147,15 +146,18 @@ plt.subplot(2, 3, 2)
 plt.barh(label_plddt, data_plddt, color="darkgreen")
 plt.xlabel("Protein Entries", fontsize=15)
 plt.title("AlphaFold pLDDT Scores", fontsize=20)
+plt.xticks(fontsize=15)
 ax = plt.gca()
-plt.yticks(fontsize=10)
-plt.text(-0.08, 1.0, "b", transform=ax.transAxes, fontsize=18, fontweight="bold")
+plt.yticks(fontsize=12)
+plt.text(-0.08, 1.0, "b", transform=ax.transAxes, fontsize=28, fontweight="bold")
 for spine in ["top", "right"]:
     ax.spines[spine].set_visible(False)
 
 plt.tight_layout()
 
-# Saves the figure as an SVG file
+# Save the figure as an SVG file
 plt.savefig("Figure 2.svg", format="svg")
-print("Figure created as Figure 2.svg")
+
+# Show the plot
+plt.show()
 
